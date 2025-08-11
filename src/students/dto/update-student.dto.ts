1292393ea401bff7f5ddf3f8b2b26a1ccdd8dfe0
Balasubramanian <123bala2006@gmail.com>
2025-08-11 15:07:17 +0530

    Create update-student.dto.ts

---
+++ src/students/dto/update-student.dto.ts
@@ -0,0 +1,31 @@
+import { IsOptional, IsString, IsEmail, IsEnum } from 'class-validator';
+
+export enum Gender {
+  Male = 'M',
+  Female = 'F',
+  Other = 'O',
+}
+
+export class UpdateStudentDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  mobile?: string;
+
+  @IsOptional()
+  @IsString()
+  photo?: string; // Base64 string
+
+  @IsOptional()
+  @IsEnum(Gender, {
+    message: 'Gender must be one of M, F, or O',
+  })
+  gender?: Gender;
+}
